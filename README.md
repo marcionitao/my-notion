297fe34d8dc919cea71d598b8ba101af740e7723
Marcio Nitao <marcionitao@gmail.com>
2023-07-22 11:07:14 +0200

    update README

--- README.md
+++ README.md
@@ -15,3 +15,8 @@
 - React Icons
 - Typescript
 - Vite
+
+### Screenshots:
+![tela_1](https://github.com/marcionitao/my-notion/assets/3647246/cb27a64b-c7ad-4667-904c-63a7d4b15512)
+![tela_2](https://github.com/marcionitao/my-notion/assets/3647246/c9dcec23-0288-49ca-9434-ca1641e08a64)
+![tela_3](https://github.com/marcionitao/my-notion/assets/3647246/e416c04e-724a-495e-91f2-8f46a71e4d7a)
--- src/components/conteudoPage.ts
+++ src/components/conteudoPage.ts
@@ -1,18 +1,18 @@
 export const conteudoPage = `
-  <h1>Vantagens e Desvantagens do Tailwind CSS em Projetos React</h1>
-  <h2>Vantagens</h2>
-  <p><strong>1. Produtividade:</strong></p>
-  <p>O Tailwind CSS é uma biblioteca de utilitários CSS que permite criar interfaces de forma rápida e eficiente. Com uma ampla variedade de classes pré-definidas, você pode estilizar componentes facilmente, sem precisar escrever CSS personalizado do zero.</p>
-  <p><strong>2. Flexibilidade:</strong></p>
-  <p>O Tailwind CSS fornece uma abordagem de design "utilities-first", onde você combina classes para criar estilos personalizados. Isso oferece uma flexibilidade enorme para personalizar o visual dos componentes, permitindo criar designs exclusivos sem se limitar a um conjunto fixo de estilos pré-definidos.</p>
-  <p><strong>3. Responsividade:</strong></p>
-  <p>O Tailwind CSS possui classes responsivas embutidas que facilitam a criação de layouts responsivos para diferentes tamanhos de tela. Com classes como <code>sm</code>, <code>md</code>, <code>lg</code> e <code>xl</code>, você pode ajustar a aparência dos componentes em dispositivos móveis, tablets e desktops.</p>
-  <pre><code class="language-javascript">console.log('Hello Tailwind!)</code></pre>
-  <h2>Desvantagens</h2>
-  <p><strong>1. Tamanho do bundle:</strong></p>
-  <p>Uma desvantagem do Tailwind CSS é que ele pode aumentar o tamanho do bundle final do seu aplicativo. Como o Tailwind CSS possui muitas classes utilitárias, todas essas classes precisam ser incluídas no arquivo CSS final, o que pode resultar em um arquivo maior do que o necessário.</p>
-  <p><strong>2. Curva de aprendizado inicial:</strong></p>
-  <p>Embora o Tailwind CSS seja poderoso, pode haver uma curva de aprendizado inicial para entender como usar efetivamente as classes utilitárias e combinar os estilos corretamente. Isso pode ser especialmente verdadeiro se você não estiver familiarizado com a abordagem "utilities-first" ou se estiver acostumado a escrever CSS personalizado.</p>
-  <p><strong>3. Personalização limitada:</strong></p>
-  <p>Embora o Tailwind CSS seja flexível, há casos em que você pode precisar de estilos mais personalizados que não são facilmente alcançados apenas com as classes utilitárias. Nesses casos, pode ser necessário escrever CSS personalizado adicional ou usar classes do Tailwind CSS em conjunto com estilos personalizados.</p>
+<h1>Advantages and Disadvantages of Using Tailwind CSS in React Projects</h1>
+<h2>Advantages</h2>
+<p><strong>1. Productivity:</strong></p>
+<p>Tailwind CSS is a utility-first CSS library that allows you to create interfaces quickly and efficiently. With a wide variety of pre-defined classes, you can style components easily without having to write custom CSS from scratch.</p>
+<p><strong>2. Flexibility:</strong></p>
+<p>Tailwind CSS provides a utilities-first design approach where you combine classes to create custom styles. This offers tremendous flexibility to customize the appearance of components, allowing you to create unique designs without being limited to a fixed set of pre-defined styles.</p>
+<p><strong>3. Responsiveness:</strong></p>
+<p>Tailwind CSS comes with built-in responsive classes that make it easy to create responsive layouts for different screen sizes. With classes like <code>sm</code>, <code>md</code>, <code>lg</code>, and <code>xl</code>, you can adjust the appearance of components on mobile devices, tablets, and desktops.</p>
+<pre><code class="language-javascript">console.log('Hello Tailwind!)</code></pre>
+<h2>Disadvantages</h2>
+<p><strong>1. Bundle Size:</strong></p>
+<p>One disadvantage of Tailwind CSS is that it can increase the final bundle size of your application. Since Tailwind CSS has many utility classes, all these classes need to be included in the final CSS file, which can result in a larger file than necessary.</p>
+<p><strong>2. Initial Learning Curve:</strong></p>
+<p>Although Tailwind CSS is powerful, there might be an initial learning curve to understand how to effectively use utility classes and combine styles correctly. This may be especially true if you are not familiar with the utilities-first approach or if you are used to writing custom CSS.</p>
+<p><strong>3. Limited Customization:</strong></p>
+<p>While Tailwind CSS is flexible, there are cases where you may need more customized styles that are not easily achieved with utility classes alone. In such cases, you may need to write additional custom CSS or use Tailwind CSS classes in combination with custom styles.</p>
 `
